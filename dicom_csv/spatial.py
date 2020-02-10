2c58949eba41ee488200390f8c50432e89ee0764
kurmukovai <kurmukovai@gmail.com>
2020-02-10 19:06:02 +0300

    fix type annotation

--- dicom_csv/spatial.py
+++ dicom_csv/spatial.py
@@ -60,19 +60,21 @@ def restore_orientation_matrix(metadata: Union[pd.Series, pd.DataFrame]):
     return metadata
 
 
-def restore_slice_location(metadata: pd.DataFrame) -> pd.DataFrame:
+def restore_slice_location(dicom_metadata: pd.DataFrame) -> pd.DataFrame:
     """Restore SliceLocation from ImagePositionPatient,
     Caution! modifies metadata inplace"""
-    coords = metadata[['ImagePositionPatient0',
+    # TODO: Fix to take into account HFS
+    coords = dicom_metadata[['ImagePositionPatient0',
                        'ImagePositionPatient1',
                        'ImagePositionPatient2',]].values
-    OM = get_orientation_matrix(metadata.iloc[0])
+    OM = get_orientation_matrix(dicom_metadata.iloc[0])
     new_coords = coords.dot(OM).astype(np.float32)
     j = np.argmax(np.std(new_coords, axis=0))
-    metadata.loc[:, 'SliceLocation'] = new_coords[:, j]
-    return metadata
+    dicom_metadata.loc[:, 'SliceLocation'] = new_coords[:, j]
+    return dicom_metadata
 
-def order_slice_locations(dicom_metadata):
+
+def order_slice_locations(dicom_metadata: pd.Series):
     return np.array(sorted(zip_equal(
         split_floats(dicom_metadata.InstanceNumbers),
         split_floats(dicom_metadata.SliceLocations),
