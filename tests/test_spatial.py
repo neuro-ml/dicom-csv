d22bd9c267453568cca7b2cfc9a7ee92fdc6cec0
kurmukovai <kurmukovai@gmail.com>
2020-10-16 22:16:45 +0300

    remove get_orientation_axis

--- tests/test_spatial.py
+++ tests/test_spatial.py
@@ -1,6 +1,6 @@
-from dicom_csv import join_tree
 from pathlib import Path
 import numpy as np
+import pandas as pd
 from dicom_csv.spatial import (
     get_orientation_matrix,
     get_image_position_patient,
@@ -16,7 +16,14 @@ from dicom_csv.spatial import (
 SERIES = '1.2.840.113619.2.374.2807.4233243.16142.1527731842.74'
 
 
+def _get_image():
+    folder = Path('/home/anvar/mri_data')
+    df = pd.read_csv(folder / 'mri_data.csv')
+    return df.query('SeriesInstanceUID == @SERIES')
+
+
 def test_get_orientation_matrix():
+    image = _get_image()
     om = get_orientation_matrix(image)
     OM = np.array([0.9882921127294, 0.03687270420588, 0.14805101688742,
                   -0.0437989943104, 0.99807987034582, 0.04379749431055]).reshape(2, 3)
@@ -27,12 +34,15 @@ def test_get_orientation_matrix():
 
 
 def test_get_image_position_patient():
+    image = _get_image()
     pos = get_image_position_patient(image)
     assert pos.shape == (216, 3)
     # TODO: add values, e.g. pos[0] check
 
 
 def test_get_slice_locations():
+    image = _get_image()
+    test_slice_loc = image.SliceLocation.values
     loc = get_slice_locations(image)
     order_loc = np.argsort(loc)
     order_test = np.argsort(test_slice_loc)
@@ -42,36 +52,34 @@ def test_get_slice_locations():
 
 
 def test_get_image_plane():
+    image = _get_image()
     plane = get_image_plane(image)
     assert plane == Plane.Axial
 
 
 def test_get_slice_spacing():
+    image = _get_image()
     spacings = _get_slices_spacing(image)
     assert spacings.shape == (215,)
     assert np.allclose(spacings.mean(), 0.8)
 
 
 def test_get_pixel_spacing():
+    image = _get_image()
     xy_spacings = get_pixel_spacing(image)
     assert xy_spacings.shape == (2,)
     assert np.allclose(xy_spacings, [0.4688, 0.4688])
 
 
 def test_get_image_size():
+    image = _get_image()
     rows, columns, slices = get_image_size(image)
     assert (rows, columns, slices) == (512, 512, 216)
 
 
 if __name__ == '__main__':
-    folder = Path('/home/anvar/mri_data')
-    df = join_tree(folder, ignore_extensions=['.ipynb'])
-    image = df.query('SeriesInstanceUID == @SERIES')
-
     test_get_orientation_matrix()
     test_get_image_position_patient()
-
-    test_slice_loc = image.SliceLocation.values
     test_get_slice_locations()
     test_get_image_plane()
     test_get_slice_spacing()
